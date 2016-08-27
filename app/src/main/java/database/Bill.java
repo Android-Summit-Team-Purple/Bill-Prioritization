ac39eb0437f3060f784c3994ba92bc060b2e4473
Mary Grimes <Mary Grimes>
2016-08-27 16:03:45 -0400

    Add account, bill, customer, merchant object

---
+++ app/src/main/java/database/Bill.java
@@ -0,0 +1,47 @@
+package database;
+
+/**
+ * A bill with an id, account id, status, payee, nickname, creation date, payment date, recurring
+ * date, upcoming payment date, and priority status.
+ */
+public class Bill {
+
+    public long id;
+    public long accountId;
+    public String status;
+    public String payee;
+    public String nickname;
+    public long creationDate;
+    public long paymentDate;
+    public long recurringDate;
+    public long upcomingPaymentDate;
+    public boolean isPriority;
+
+    public Bill(long id, long accountId, String status, String payee, String nickname, long creationDate, long paymentDate, long recurringDate, long upcomingPaymentDate) {
+        this.id = id;
+        this.accountId = accountId;
+        this.status = status;
+        this.payee = payee;
+        this.nickname = nickname;
+        this.creationDate = creationDate;
+        this.paymentDate = paymentDate;
+        this.recurringDate = recurringDate;
+        this.upcomingPaymentDate = upcomingPaymentDate;
+    }
+
+    public Bill(long accountId, String status, String payee, String nickname, long creationDate, long paymentDate, long recurringDate, long upcomingPaymentDate) {
+        this.accountId = accountId;
+        this.status = status;
+        this.payee = payee;
+        this.nickname = nickname;
+        this.creationDate = creationDate;
+        this.paymentDate = paymentDate;
+        this.recurringDate = recurringDate;
+        this.upcomingPaymentDate = upcomingPaymentDate;
+        this.isPriority = calculatePriority();
+    }
+
+    private boolean calculatePriority() {
+        return true;
+    }
+}
