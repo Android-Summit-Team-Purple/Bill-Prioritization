e0bd8f3508d5c5f1a45fc1b17525cb82033a8b6f
electrostat <agbyaj@gmail.com>
2016-08-27 14:14:21 -0400

    afford bills calculation
    - calculates if your current balance can cover all of your bills

--- app/src/main/java/teampurple/com/bill_prioritization/MainActivity.java
+++ app/src/main/java/teampurple/com/bill_prioritization/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ManagementLogic managementLogic = new ManagementLogic();
-        managementLogic.getBalance();
+//        ManagementLogic managementLogic = new ManagementLogic();
+//        managementLogic.getBalance();
     }
 }
