866e692ee119ebb0deecf1b64459f34b50cf35bf
electrostat <agbyaj@gmail.com>
2016-08-27 13:32:55 -0400

    Get Balance
    - get user account balance

--- app/src/main/java/teampurple/com/bill_prioritization/MainActivity.java
+++ app/src/main/java/teampurple/com/bill_prioritization/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ManagementLogic managementLogic = new ManagementLogic();
+        managementLogic.getBalance();
     }
 }
